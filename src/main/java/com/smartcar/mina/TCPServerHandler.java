836727514ef2f4ddeceea3bb63de281bacdbf845
jeanskris <823509241@qq.com>
2016-05-24 22:36:15 +0800

    change some details

--- src/main/java/com/smartcar/mina/TCPServerHandler.java
+++ src/main/java/com/smartcar/mina/TCPServerHandler.java
@@ -3,6 +3,7 @@ package com.smartcar.mina;
 import com.smartcar.service.interfaces.ICarInfoService;
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,6 +21,7 @@ public class TCPServerHandler extends IoHandlerAdapter {
 
     @Override
     public void messageReceived(IoSession session, Object message) throws Exception {
+        System.out.println("data from client:"+message);
         if (message instanceof IoBuffer) {
             IoBuffer buffer = (IoBuffer) message;
             System.out.println(buffer.toString());
@@ -27,6 +29,7 @@ public class TCPServerHandler extends IoHandlerAdapter {
             buffer.setAutoExpand(true);
             byte[] bytes = new byte[buffer.limit()];
             buffer.get(bytes);
+
             if(bytes[2]==0x30) {
                 carInfoService.updatePower(bytes);//调用service业务处理
             }
@@ -43,5 +46,21 @@ public class TCPServerHandler extends IoHandlerAdapter {
     public void sessionCreated(IoSession session) throws Exception {
         super.sessionCreated(session);
         System.out.println("Server created a session: " + session.getRemoteAddress());
+    }
+    @Override
+    public void sessionClosed(IoSession session) throws Exception {
+        System.out.println("Server sessionClosed" );
+    }
+
+    @Override
+    public void sessionIdle(IoSession iosession, IdleStatus idlestatus)
+            throws Exception {
+        System.out.println("sessionIdle");
+        super.sessionIdle(iosession, idlestatus);
+    }
+    @Override
+    public void sessionOpened(IoSession iosession) throws Exception {
+        System.out.println("sessionOpened");
+        super.sessionOpened(iosession);
     }
 }
